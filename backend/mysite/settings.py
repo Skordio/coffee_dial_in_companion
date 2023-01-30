0b6cdd63f83667f70149ef91503fe57346a23238
Steven Wells <snw2535@g.rit.edu>
2023-01-29 23:17:52 -0500

    feat: added new database tables and Django models

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'root',
+        'PASSWORD': 'Decade-Tarmac-Fraction-Trickery8-Civil',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT-5'
 
 USE_I18N = True
 
