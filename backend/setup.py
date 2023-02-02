5a04ea373c1c945de8763bdeb58b99485beab8c7
Steven Wells <skordion@hotmail.com>
2023-02-02 09:25:06 -0500

    feat: added black and stuff for formatting

--- backend/setup.py
+++ backend/setup.py
@@ -3,4 +3,11 @@ from setuptools import setup, find_packages
 
 setup(name='myproject',
       version='1.0',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+        "Django==4.1.5",
+        "django-sql-utils",
+        "pylint",
+        "black",
+        "djangorestframework-simplejwt",
+        "cryptography"])
