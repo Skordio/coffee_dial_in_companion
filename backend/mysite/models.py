dff8b904df74561bed1dd9758be118477f743fbe
Skordio <skordion@hotmail.com>
2023-04-15 16:00:32 -0400

    chore: changed a few column names for clarity

--- backend/mysite/models.py
+++ backend/mysite/models.py
@@ -3,15 +3,27 @@ from django.contrib.auth.models import User
 # COFFEE MODELS
 
 class Cof(models.Model):
-    cof_id = models.UUIDField(db_column='COF_ID', primary_key=True)  # Field name made lowercase.
-    cof_dateroasted = models.DateField(db_column='COF_DateRoasted', blank=True, null=True)  # Field name made lowercase.
-    cof_datebestby = models.DateField(db_column='COF_DateBestBy', blank=True, null=True)  # Field name made lowercase.
-    cof_name = models.CharField(db_column='COF_Name', max_length=100) # Field name made lowercase.
-    cof_user = models.ForeignKey(User, models.DO_NOTHING, db_column='COF_User')  # Field name made lowercase.
+    cof_id = models.UUIDField(primary_key=True)  # Field name made lowercase.
+    date_roasted = models.DateField(blank=True, null=True)  # Field name made lowercase.
+    date_best_by = models.DateField(blank=True, null=True)  # Field name made lowercase.
+    name = models.CharField( max_length=100) # Field name made lowercase.
+    user = models.ForeignKey(User, models.DO_NOTHING, db_column='COF_User')  # Field name made lowercase.
 
     class Meta:
         managed = True
         db_table = 'cof'
+
+
+class CofPull(models.Model):
+    cofpull_id = models.UUIDField(primary_key=True)
+    grams_in = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    grams_out = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    duration_seconds = models.IntegerField(blank=True, null=True)
+    cof_id = models.ForeignKey(Cof, models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'cof_pull'
 
 
 class CofComment(models.Model):
@@ -25,15 +37,3 @@ class CofComment(models.Model):
     class Meta:
         managed = True
         db_table = 'cof_comment'
-
-
-class CofPull(models.Model):
-    cofpull_id = models.UUIDField(primary_key=True)
-    grams_in = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
-    grams_out = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
-    duration_seconds = models.IntegerField(blank=True, null=True)
-    cof_id = models.ForeignKey(Cof, models.DO_NOTHING)
-
-    class Meta:
-        managed = True
-        db_table = 'cof_pull'
